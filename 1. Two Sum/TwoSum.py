e0af5b2a378443fb0eb15bd40602341e81a3da21
Kuba <kubalew.pl@gmail.com>
2024-04-03 16:42:42 +0200

    changed file structure, added Readme

---
+++ 1. Two Sum/TwoSum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = target-nums[i]
+            if
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+
+print(Solution().twoSum([2,7,11,15]), 9)
---
+++ README.md
@@ -0,0 +1,9 @@
+# [Leetcode](https://leetcode.com/) Problems
+
+## Description
+
+This is my repository for all my Leetcode problems codes.
+
+Most of them are made with python and some of them with Javascript
+
+### Link to my [Leetcode profile](https://leetcode.com/lewusdaprogrammer/)
